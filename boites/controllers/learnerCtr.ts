c8a607a148a93e98fce4931380b8e71b887b036c
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-03 12:51:15 +0100

    Fix : Change error passwod to password in adminCtr

--- boites/controllers/learnerCtr.ts
+++ boites/controllers/learnerCtr.ts
@@ -1,7 +1,7 @@
 import learnerModel from '../models/learnerModel'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import bcrypt from 'bcrypt'
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken'
 
 type Data = {
   message: String
@@ -45,7 +45,16 @@ export async function createOrFindLearner(
         password,
       } = request.body
 
-      if (name && lastName && image && email && option && description && promotion && password) {
+      if (
+        name &&
+        lastName &&
+        image &&
+        email &&
+        option &&
+        description &&
+        promotion &&
+        password
+      ) {
         const hash = bcrypt.hashSync(password, 10)
         learnerModel
           .create({
@@ -79,125 +88,135 @@ export async function createOrFindLearner(
 
 export async function DeleteLearner(request: NextApiRequest, response: NextApiResponse) {
   const id = request.query.id
-  try{
-      learnerModel.findByIdAndRemove(id)
+  try {
+    learnerModel
+      .findByIdAndRemove(id)
       .then(learner => {
-          if(!learner) {
-            response.status(500).json({message: "Learner you wanna delete doesn't exist"})
-          }
-          else{
-            response.status(200).json({message: "Learner deleted"})
-          }
+        if (!learner) {
+          response.status(500).json({ message: "Learner you wanna delete doesn't exist" })
+        } else {
+          response.status(200).json({ message: 'Learner deleted' })
+        }
       })
       .catch(err => {
         throw err
       })
-  } 
-  catch(err) {
+  } catch (err) {
     throw err
   }
 }
 
-export async function UpdateLearner(request: NextApiRequest, response: NextApiResponse<Data>) {
-    const id = request.query.id
-    const { name, lastName, image, email, password, 
-            contact, option, description, promotion, 
-            status} = request.body
+export async function UpdateLearner(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>,
+) {
+  const id = request.query.id
+  const {
+    name,
+    lastName,
+    image,
+    email,
+    password,
+    contact,
+    option,
+    description,
+    promotion,
+    status,
+  } = request.body
 
-    if(!name && name.trim() === "") {
-      response.status(500).json({message: "Name cannot be empty", data: name})
-    }
-    else if(!lastName && lastName.trim() === "")
-      response.status(500).json({message: "LastName cannot be empty", data: lastName})
-    else if(!image && image.trim() === "")
-      response.status(500).json({message: "Image cannot be empty", data: image})
-    else if(!email && email.trim() === "")
-      response.status(500).json({message: "Email cannot be empty", data: email})
-    else if(!password && password.trim() === "")
-      response.status(500).json({message: "Password cannot be empty", data: password})
-    else if(!option && option.trim() === "")
-      response.status(500).json({message: "Option cannot be empty", data: option})
-    else if(!promotion && promotion.trim() === "")
-      response.status(500).json({message: "Promotion cannot be empty", data: promotion})
-    else {
-      bcrypt.hash(request.body.password, 10, async function(err, hash) {
-        try{
-          await learnerModel.findByIdAndUpdate(id, {
-             name, 
-             lastName, 
-             image, 
-             email, 
-             password: hash, 
-             contact, 
-             option, 
-             description, 
-             promotion, 
-             status
-           })
-           .then(learner=> {
-               if(!learner){
-                 response.status(500).json({message: "Learner you wanna update doesn't exits", data: learner})
-               }
-               else{
-                 response.status(200).json({message: "Learner updated", data: null})
-               }
-           })
-           .catch(err =>{
-             throw err
-           })
-         }
-         catch(err){
-           throw err
-         }
-    });
-    
-    }
+  if (!name && name.trim() === '') {
+    response.status(500).json({ message: 'Name cannot be empty', data: name })
+  } else if (!lastName && lastName.trim() === '')
+    response.status(500).json({ message: 'LastName cannot be empty', data: lastName })
+  else if (!image && image.trim() === '')
+    response.status(500).json({ message: 'Image cannot be empty', data: image })
+  else if (!email && email.trim() === '')
+    response.status(500).json({ message: 'Email cannot be empty', data: email })
+  else if (!password && password.trim() === '')
+    response.status(500).json({ message: 'Password cannot be empty', data: password })
+  else if (!option && option.trim() === '')
+    response.status(500).json({ message: 'Option cannot be empty', data: option })
+  else if (!promotion && promotion.trim() === '')
+    response.status(500).json({ message: 'Promotion cannot be empty', data: promotion })
+  else {
+    bcrypt.hash(request.body.password, 10, async function (err, hash) {
+      try {
+        await learnerModel
+          .findByIdAndUpdate(id, {
+            name,
+            lastName,
+            image,
+            email,
+            password: hash,
+            contact,
+            option,
+            description,
+            promotion,
+            status,
+          })
+          .then(learner => {
+            if (!learner) {
+              response
+                .status(500)
+                .json({
+                  message: "Learner you wanna update doesn't exits",
+                  data: learner,
+                })
+            } else {
+              response.status(200).json({ message: 'Learner updated', data: null })
+            }
+          })
+          .catch(err => {
+            throw err
+          })
+      } catch (err) {
+        throw err
+      }
+    })
+  }
 }
 
-export async function FoundOneLearner(request: NextApiRequest, response: NextApiResponse<Data>){
-    const id = request.query.id
+export async function FoundOneLearner(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>,
+) {
+  const id = request.query.id
 
-    try{
-      await learnerModel.findById(id)
-      .then(learner => {
-          if(!learner){
-              response.status(400).json({ message: "Learner Not Found", data: null })
-          }
-          else{
-            response.status(200).json({ message: "Learner Found succesfully", data: learner })
-          }
-      })
-    }
-    catch(err){
-      throw err
-    }
+  try {
+    await learnerModel.findById(id).then(learner => {
+      if (!learner) {
+        response.status(400).json({ message: 'Learner Not Found', data: null })
+      } else {
+        response.status(200).json({ message: 'Learner Found succesfully', data: learner })
+      }
+    })
+  } catch (err) {
+    throw err
+  }
 }
 
-export async function LoginLearner(request: NextApiRequest, response: NextApiResponse){
-    const user = await learnerModel.findOne({ email: request.body.email})
+export async function LoginLearner(request: NextApiRequest, response: NextApiResponse) {
+  const user = await learnerModel.findOne({ email: request.body.email })
 
-    try{
-      if(!user){
-        response.status(400).json({ message: "Email or Password incorrect" })
-      }
-      else{
-        const valid = await bcrypt.compare(request.body.password, user.password)
-          if(!valid){
-              response.status(400).json({ message: "Email or Password incorrect" })
-          }
-          else{
-            response.status(200).json({ 
-                data: user,
-                token: "Baerer " + jwt.sign(
-                  { LearnerId: user._id },
-                  "RANDOM_TOKEN_SECRET",
-                  { expiresIn: "24h" }
-                  )
-             })
-          }
+  try {
+    if (!user) {
+      response.status(400).json({ message: 'Email or Password incorrect' })
+    } else {
+      const valid = await bcrypt.compare(request.body.password, user.password)
+      if (!valid) {
+        response.status(400).json({ message: 'Email or Password incorrect' })
+      } else {
+        response.status(200).json({
+          data: user,
+          token:
+            'Baerer ' +
+            jwt.sign({ LearnerId: user._id }, 'RANDOM_TOKEN_SECRET', {
+              expiresIn: '24h',
+            }),
+        })
       }
     }
-    catch(err){
-      throw err
-    }
+  } catch (err) {
+    throw err
+  }
 }
