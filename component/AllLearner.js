ad7b60fc19ab9f6958b60f37a56864f3942c66d2
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-06 17:32:59 +0100

    feat: Add displayLearner.js

--- component/AllLearner.js
+++ component/AllLearner.js
@@ -1,4 +1,7 @@
 import React from 'react'
+// import Learners from '../data/learners'
+import Image from 'next/image'
+import profil from '../img/main.jpg'
 
 export default function DisplayLeaner() {
   return (
@@ -10,14 +13,68 @@ export default function DisplayLeaner() {
             Contacter un developpeur(se) web ou un marketeur(se) digital qui vous convient
             pour que votre entreprise soit numerisée comme vous y pensez.
           </p>
-          <div className='inputContaire'>
+          <div className="inputContaire">
             <input className="input" placeholder="Recherche par nom" type="text" />
-            <input className="input" placeholder="Recherche par cohorte:2022" type="text" />
+            <input
+              className="input"
+              placeholder="Recherche par cohorte:2022"
+              type="text"
+            />
           </div>
         </div>
       </div>
-      <section>
-        
+      <section className="sectionLearner">
+        <div>
+          <h1 className="title">
+            Developpeurs Web/mobile et Specialiste en Marketing Digital
+          </h1>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel obcaecati nihil
+            aspernatur culpa sed eius ullam eligendi, excepturi deleniti consequatur illo
+            dolorem dicta, enim, similique aut optio? Hic, tempora temporibus 
+          </p>
+        </div>
+        <ul className="containerItem">
+          {/* {Learners.map((learner, index) => {
+            return <li key={index}> {learner.name} </li>
+          })} */}
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+          <li className="item hover">
+            <Image src={profil} alt="profil" className="profil" />
+            <div>nom</div>
+            <div>option</div>
+            <div>status</div>
+          </li>
+        </ul>
       </section>
     </main>
   )
---
+++ data/learners.js
@@ -0,0 +1,12 @@
+export const Learners = [
+  {
+    name: 'bleudy kitondo',
+    option: 'dev',
+    status: 'sans emploi',
+  },
+  {
+    name: 'jeannot diambu',
+    option: 'dev',
+    status: 'travaille chez ITM',
+  },
+]
