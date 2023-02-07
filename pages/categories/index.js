32bd3e180d6d221c74ae2b1de257a25508ec7b67
jeannot diambu <diambujeannot@gmail.com>
2023-02-07 12:00:07 +0100

    Refactor : Add structure categories page

---
+++ pages/categories/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../component/Header";
+import Footer from "../../component/Footer";
+import DisplayLeaner from "../../component/AllLearner";
+
+function index() {
+  return (
+    <>
+      <Header />
+      <DisplayLeaner />
+      <Footer />
+    </>
+  );
+}
+
+export default index;
