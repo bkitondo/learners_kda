bbec86c0bbeb8b79d51902111dfa8ea90bea6670
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-08 15:54:59 +0100

    refactor: add file signin

---
+++ pages/signin/index.js
@@ -0,0 +1,13 @@
+import Header from '../../component/Header'
+import Footer from '../../component/Footer'
+import SignIn from '../../component/SingIn'
+
+export default function SignInPage() {
+  return (
+    <>
+      <Header />
+      <SignIn />
+      <Footer />
+    </>
+  )
+}
