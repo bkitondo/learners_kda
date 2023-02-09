caac16684ff06d7d0cdf8e899277ece0de54d6e8
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-09 15:16:31 +0100

    feat: add sign in with google

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,8 @@ export default function Home() {
       </Head>
       <div>
         <Header />
-        <Main />
-        <Learner />
+        {/* <Main /> */}
+        {/* <Learner /> */}
         {/* <DispalyLeaner /> */}
         <SignIn/>
         <Footer />
