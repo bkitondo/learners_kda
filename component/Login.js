2fa91d9a6e7abf5566a522a230b4d8b6d8d0f62d
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-15 12:09:53 +0100

    refactor: add animation at the login and signin Page

--- component/Login.js
+++ component/Login.js
@@ -5,12 +5,8 @@ import google from '../img/google.png'
 export default function Login() {
   return (
     <main>
-      <div className="formContainer">
+      <div className="formContainer" data-aos="zoom-in" data-aos-duration="2000">
         <div className="fromtitle">Connectez-vous !</div>
-        {/* <div className="display">
-          <input className="formBtn" type="text" placeholder="Nom" />
-          <input className="formBtn" type="text" placeholder="Prénom" />
-        </div> */}
         <input className="formInput" type="email" placeholder="Email" />
         <input className="formInput" type="password" placeholder="Mot de passe" />
         <div className="display">
@@ -18,11 +14,9 @@ export default function Login() {
           <button className="formBtn  submit">Connexion</button>
         </div>
       </div>
-      <div className='containerGoogle'>
-        <Image src={google} alt='logo de google'className='logo' />
-        <div className='textGoogle'>
-          Se connecter  avec son compte google
-        </div>
+      <div className="containerGoogle" data-aos="zoom-in" data-aos-duration="3000">
+        <Image src={google} alt="logo de google" className="logo" />
+        <div className="textGoogle">Se connecter avec son compte google</div>
       </div>
     </main>
   )
