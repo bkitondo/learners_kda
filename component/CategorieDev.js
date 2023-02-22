dac4d2ebc8c31df4410b941ec10d05158361da1c
jeannot diambu <diambujeannot@gmail.com>
2023-02-22 15:17:21 +0100

    Refactor : add name of learner

--- component/CategorieDev.js
+++ component/CategorieDev.js
@@ -9,7 +9,7 @@ function CategorieDev({smd}) {
     <Link href="retail">
       <Image src={smd.image} width={210} height={240} alt="profil" className="profil" />
       <div className="containerDetail">
-        <div className="itemdetail">{smd.name}</div>
+        <div className="itemdetail">{smd.name} {smd.lastName}</div>
         <div className="itemdetail">{smd.option}</div>
         <div className="itemdetail">{smd.status}</div>
       </div>
