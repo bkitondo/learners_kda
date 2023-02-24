c6865de46c835e29c58e9e6f338f85b1df70bb3d
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-02-24 13:36:49 +0100

    fix: fix navigate to retail

--- component/CategorieDev.js
+++ component/CategorieDev.js
@@ -1,37 +1,22 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import Image from "next/image";
-import profil from "../img/main.jpg";
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import profil from '../img/main.jpg'
 
 function CategorieDev({ smd }) {
-  const [learner, setLearner] = useState();
-  function handlerDev() {
-    setLearner(smd);
-  }
-
-  console.log(learner);
-
   return (
-    <li className="item hover" key={smd._id} onClick={handlerDev}>
-      <Link href="#">
-        <Image
-          src={smd.image}
-          width={210}
-          height={240}
-          alt="profil"
-          className="profil"
-        />
+    <li className="item hover" key={smd._id}>
+      <Link href="../retail">
+        <Image src={smd.image} width={210} height={240} alt="profil" className="profil" />
         <div className="containerDetail">
-          <div className="itemdetail">
-            {smd.name} {smd.lastName}
-          </div>
+          <div className="itemdetail">{smd.name}</div>
           <div className="itemdetail">{smd.option}</div>
           <div className="itemdetail">{smd.status}</div>
         </div>
         <div className="detail">détail</div>
       </Link>
     </li>
-  );
+  )
 }
 
-export default CategorieDev;
+export default CategorieDev
